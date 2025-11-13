03477d97ffbc22e05086bc87a7a4c3166f90b5b6
irg138 <irg138.1@yandex.com>
2025-11-13 19:07:14 +0200

    Update wsgi.py  delete WhiteNoise

--- zvit_generator/wsgi.py
+++ zvit_generator/wsgi.py
@@ -4,6 +4,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zvit_generator.settings')
 application = get_wsgi_application()
 
 
-from whitenoise import WhiteNoise
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
-
