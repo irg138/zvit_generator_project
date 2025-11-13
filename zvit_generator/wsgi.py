b773250c7a16a21f42446ea4276d1bc927a77a36
irg138 <irg138.1@yandex.com>
2025-11-13 18:38:07 +0200

    Update wsgi.py   добавлен WhiteNoise

--- zvit_generator/wsgi.py
+++ zvit_generator/wsgi.py
@@ -2,3 +2,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zvit_generator.settings')
 application = get_wsgi_application()
+
+
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
